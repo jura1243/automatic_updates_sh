86857a44a5a7e68ace7b38015acf281375b91e5e
jura1243 <jurawww@yandex.ru>
2024-05-23 18:19:07 +0300

    Update showforupdate.sh

--- showforupdate.sh
+++ showforupdate.sh
@@ -1,5 +1,7 @@
 #show available updates
 pathtocomposer=~/.local/bin
+folderofdrupal=~/dev.zoroastrian.ru
 
+cd $folderofdrupal
 $pathtocomposer/composer update -W --dry-run
 read -n1 -r -p "Press any key to continue..." key
