8adc6669c01fb24c503dcccfe2cdb43e7abd7513
Yuri Ivanov <jurawww@yandex.ru>
2024-12-19 04:32:47 +0300

    Update checkforupdates.sh
    удалил php перед drush,
    поменял названия переменных

--- checkforupdates.sh
+++ checkforupdates.sh
@@ -9,7 +9,7 @@
 
 #tune paths
 #which php
-pathtophp=/usr/local/php/cgi/8.2/bin
+pathtophp=/usr/local/php/cgi/8.3/bin
 #which composer
 pathtocomposer=~/.local/bin
 sendmailwithpath=$(which sendmail)
@@ -23,14 +23,20 @@ noemail=0
 #0-upgrade 1-dont upgrade
 noupgrade=0
 
+#1-send email 0-dont send
+email=1
+#1-upgrade 0-dont upgrade
+upgrade=1
+
+
 #begin of the program
 cd $folderofdrupal
 
-$pathtophp/php $pathtocomposer/composer update -W --dry-run &>$folderofdrupal/automatic_updates_sh/file
+$pathtocomposer/composer update -W --dry-run &>$folderofdrupal/automatic_updates_sh/file
 if grep  Upgrading  $folderofdrupal/automatic_updates_sh/file; then
 
 #to send email
-if [ $noemail -eq 0 ]; then
+if [ $email -eq 1 ]; then
     echo -e "Content-Type: text/plain\r\nFrom: $emailfrom\r\nSubject: Available updates\r\n\r\n"|cat - $folderofdrupal/automatic_updates_sh/file| $sendmailwithpath -f $emailfrom $emailto
     retVal=$?
     if [ $retVal -ne 0 ]; then
@@ -41,17 +47,17 @@ if [ $noemail -eq 0 ]; then
 fi
 
 #to update the site
-if [ $noupgrade -eq 0 ]; then
-    $pathtophp/php $folderofdrupal/vendor/bin/drush state:set system.maintenance_mode 1 --input-format=integer
-    $pathtophp/php $pathtocomposer/composer update -W
-    $pathtophp/php $folderofdrupal/vendor/bin/drush updatedb --cache-clear --yes
+if [ $upgrade -eq 1 ]; then
+    $folderofdrupal/vendor/bin/drush state:set system.maintenance_mode 1 --input-format=integer
+    $pathtocomposer/composer update -W
+    $folderofdrupal/vendor/bin/drush updatedb --cache-clear --yes
     retVal=$?
     if [ $retVal -ne 0 ]; then
         echo "updb error"
         echo -e "Content-Type: text/plain\r\nFrom: $emailfrom\r\nSubject: error on updb\r\n\r\n"|echo "error on updb\r\n from checkforupdates.sh"| $sendmailwithpath -f $emailfrom $emailto
     fi
-    $pathtophp/php $folderofdrupal/vendor/bin/drush cache:rebuild
-    $pathtophp/php $folderofdrupal/vendor/bin/drush state:set system.maintenance_mode 0 --input-format=integer
+    $folderofdrupal/vendor/bin/drush cache:rebuild
+    $folderofdrupal/vendor/bin/drush state:set system.maintenance_mode 0 --input-format=integer
 fi
 
 else
